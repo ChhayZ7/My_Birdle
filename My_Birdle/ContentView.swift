31c09857f698d64e1252e9975c014b66b343d17e
Kimchhay Leng <150762525+ChhayZ7@users.noreply.github.com>
2025-11-06 17:36:20 +1030

    Update ContentView.swift

--- My_Birdle/ContentView.swift
+++ My_Birdle/ContentView.swift
@@ -8,17 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSplash = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            if showSplash {
+                SplashView(showSplash: $showSplash)
+            } else {
+                MainMenuView()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
